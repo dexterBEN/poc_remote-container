783cbcd165f7714ed131d2360f1fb515561c48c0
dexterBEN <bdextereric@yahoo.fr>
2019-12-06 15:51:22 +0000

    just init the project

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,5 +10,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Remote World!" );
+        System.out.println( "Test commint Julien!" );
+
     }
 }
---
+++ src/main/java/com/mycompany/app/Truc.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Truc
+ */
+public class Truc {
+
+    public static String truc() {
+        System.out.println("  ");
+        String tt = "jdjdjdjdjjd";
+        System.out.print(tt);
+        return tt;
+    }
+}
